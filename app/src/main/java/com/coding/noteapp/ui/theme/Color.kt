8960701ca71c9b0384cdec708c7618992adb5d5a
ThorsPowa <nwag7879@gmail.com>
2022-12-04 11:04:39 -0500

    Initial Commit

--- app/src/main/java/com/coding/noteapp/ui/theme/Color.kt
+++ app/src/main/java/com/coding/noteapp/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.plcoding.cleanarchitecturenoteapp.ui.theme
+package com.coding.noteapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/test/java/com/coding/noteapp/ExampleUnitTest.kt
+++ app/src/test/java/com/coding/noteapp/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.cleanarchitecturenoteapp
+package com.coding.noteapp
 
 import org.junit.Test
 
